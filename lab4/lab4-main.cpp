b7a0774e7a733534475818300246e705c970c1c1
minako-aino <79149650+minako-aino@users.noreply.github.com>
2021-03-30 22:35:37 +0300

    Update lab4-main.cpp

--- lab4/lab4-main.cpp
+++ lab4/lab4-main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <string.h>
+#include "lab4-p1.cpp"
 #include "lab4-p2.cpp"
 
 using namespace std;
 
 int main(){
-	main_1();
+	int answ;
+	cout << "Choose your method:\n1 - array (ring queue)\n2 - list\n\n" << "ENTER > ";
+	cin >> answ;
+	switch(answ){
+		case 1: {
+			main_1();
+			break; }
+		case 2: {
+			main_2();
+			break; }
+		default:
+			cout << "Wrong command";
+	}
+	
 	return 0;
 }
