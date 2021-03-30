56ed479d4a0f18d1ac50236dab6982cff55c0497
minako-aino <79149650+minako-aino@users.noreply.github.com>
2021-03-30 21:06:06 +0300

    README is included
    Алгоритм
    Пока есть ещё символы для чтения:
    1. Читаем очередной символ.
       - Если символ является числом, добавляем его к выходной строке..
       - Если символ является открывающей скобкой, помещаем его в стек.
       - Если символ является закрывающей скобкой:
          До тех пор, пока верхним элементом стека не станет открывающая скобка, выталкиваем элементы из стека в выходную строку. При этом открывающая скобка удаляется из стека, но в выходную строку не добавляется. Если стек закончился раньше, чем мы встретили открывающую скобку, это означает, что в выражении либо неверно поставлен разделитель, либо не согласованы скобки.
       - Если символ является оператором о1, тогда:
          1) пока приоритет o1 меньше либо равен приоритету оператора, находящегося на вершине стека выталкиваем верхние элементы стека в выходную строку;
          2) помещаем оператор o1 в стек.
    Когда входная строка закончилась, выталкиваем все символы из стека в выходную строку. В стеке должны были остаться только символы операторов; если это не так, значит в выражении не согласованы скобки.

--- lab4/lab4-stack.cpp
+++ lab4/lab4-stack.cpp
@@ -3,12 +3,21 @@
 
 using namespace std;
 
+void push(char *_stack, int &_ptr, char data) {
+	_stack[_ptr++] = data;
+}
+
+char pop(char *_stack, int &_ptr) {
+	return _stack[--_ptr];
+}
+
 void push(char *_stack, int &_ptr, char data);
 char pop(char *_stack, int &_ptr);
 
 void PostfixNotation(const char *_infix, char *_postfix) {
 	// stack for operators
-	char stack[100];	
+	char stack[100];
+	// head of stack	
 	int st_ptr = 0;
 	
 	// index in output string
@@ -38,15 +47,14 @@ void PostfixNotation(const char *_infix, char *_postfix) {
 						break;
 				}
 				cout << "Add op to stack(+ or -)\n";
-				push(stack, st_ptr, c);
+				push(stack, st_ptr, current);
 				break; }
 			case '*':
 			case '/': {
 				// pop all operators with greater or equal precedence from the stack into the output line
 				while (st_ptr != 0) {
 					char op = stack[st_ptr-1];
-					if ((op == '^') || (op == '*') || (op == '/'))
-					{
+					if ((op == '^') || (op == '*') || (op == '/')) {
 						cout << "Add * or / to the output\n";
 						_postfix[out_index++] = op;	
 						cout << "Delete * or / from stack\n";
@@ -56,11 +64,11 @@ void PostfixNotation(const char *_infix, char *_postfix) {
 						break;
 				}
 				cout << "Add * or / to the stack\n";
-				push(stack, st_ptr, c);
+				push(stack, st_ptr, current);
 				break; }
 			case '(': {
 				cout << "Put ( to a stack\n";
-				push(stack, st_ptr, c);
+				push(stack, st_ptr, current);
 				break; }
 			case ')':
 			// pop all the elements before '(' and just drop it
@@ -79,7 +87,7 @@ void PostfixNotation(const char *_infix, char *_postfix) {
 			// if there is a letter/number
 			default:
 				cout << "Add a letter to the output\n";
-				_postfix[out_index] = c;
+				_postfix[out_index] = current;
 				out_index++;
 				break;
 		}
@@ -95,24 +103,19 @@ void PostfixNotation(const char *_infix, char *_postfix) {
 	_postfix[out_index] = 0;
 }
 
+void calculate(char *array){
+	int size = sizeof(array);
+	char str_postfix[size];
 
-void push(char *_stack, int &_ptr, char data) {
-	_stack[_ptr++] = data;
+	cout << "\nINPUT: "<< array << endl;
+	cout << "PROGRESS: \n";
+	PostfixNotation(array, str_postfix);
+	cout << "RESULT: "<< str_postfix << endl;
 }
-
-char pop(char *_stack, int &_ptr) {
-	return _stack[--_ptr];
-}
-
 
 int main() {
-	char str_infix[] = "D/(A-B-C)-E*F";
-	char str_postfix[100];
-
-	cout << str_infix << endl;
-
-	PostfixNotation(str_infix, str_postfix);
-	cout << str_postfix << endl;
+	char* str_infix[4] = {"D/(A-B-C)-E*F", "(A+B)*C-(D+E)/F", "A/(B-C)+D*(E-F)", "(A*B+C)/D-F/E"};
+	for(int i=0; i < 4; i++) calculate(str_infix[i]);
 
 	return 0;
 }
