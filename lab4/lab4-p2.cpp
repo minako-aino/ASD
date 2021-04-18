c915e7da89298394a2ecc63367480d7966a13bc5
minako-aino <79149650+minako-aino@users.noreply.github.com>
2021-04-18 19:05:07 +0300

    gospodi pomogi

--- lab4/lab4-p2.cpp
+++ lab4/lab4-p2.cpp
@@ -1,127 +1,69 @@
 // part 2 - list
 #include <iostream>
-#include <string.h>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
 
 using namespace std;
- 
-struct Node {
-    int data;
-    Node *next;
-};
 
-class Queue {
+int main(){
 	int size;
-    Node *first;
-    Node *last;
-public:
-	~Queue();
-	int size_get();
-	void size_set(int x);
-	bool is_empty();
-	int show_first();
-	void add_el();
-	void show_List();
-	void creation();
-	void delete_el();
-};
-
-// getter for size value
-int Queue::size_get(){
-	return size;
-}
-
-// and setter as well
-void Queue::size_set(int x){
-	size = x;
-}
-
-// destructor
-Queue::~Queue() {
-     while (first) {
-         last = first->next;             
-         delete first;                   
-         first = last;                   
-     }
- }
-
-// check whether the list is empty
-bool Queue::is_empty() {
-	if (size==0) return true;
-    else return false;
-}
-
-int Queue::show_first() {
-    return first->data;
-}
-
-// add elements
-void Queue::add_el() {
-    int value;
-    // case 1 - we need to create a new list
-    if(is_empty() == true){
-    	cout << "\nValue > "; 
-    	first = new Node;
-    	cin >> value;
-    	first->data = value;
-    	first->next = NULL;
-    	last = first;
-		cout << "\nIt was added\n\n";  
-    	size = 1;
-    // case 2 - we already have the first element
-	} else {
-		cout << "\nValue > "; 
-		cin >> value;
-    	last->next = new Node;
-    	last = last->next;
-    	last->data = value;
-    	last->next = NULL; 
-    	cout << "\nIt was added\n\n"; 
-		size++;}
-}
- 
-void Queue::show_List(){
-	struct Node *h;
-	if(is_empty() == true) {
-    	printf("It is empty!\n");
-    	return;
-  	}
-	for(h = first; h!= NULL; h=h->next)
-    	cout << h->data << " ";
-}
-
-// delete first element
-void Queue::delete_el(){
-    Node *curNode = first;
-	first = first->next;
-    delete curNode;		                                   
-}
-
-int main_2() {
-    Queue Q;
-    Q.size_set(0);
-    char number;
+	int answ;
+	int data;
+	int head;
+	int tail;
+	int current;
+	
+	cout << "How many elements do you want to have?\n";
+    cin >> size;
+	int *arr = new int[size];
+    head = tail = 0;
     do {
-        cout << "\n1. ADD\n2. SHOW CURRENT STATE\n3. DELETE ELEMENT\n0. QUIT\n\n" << endl;
-        cout << "Enter > "; 
-		cin >> number;
-        switch (number) {
-		case '1': {
-        	Q.add_el();
-        	Q.show_List();
-            break; }
-        case '2': {
-			Q.show_List();
-            break; }
-        case '3':{
-        	Q.delete_el();
-        	Q.show_List();
-			break; }
-		case '0': break;
-        default: cout << endl << "Wrong command\n\n";
-            break;
-        }
-    } while (number != '0');
-    
-	system("pause");
-    return 0;
+    	cout << "1. CHECK THE STATUS\n2. DELETE ELEMENT\n3. ADD NEW\n0. QUIT\n" << "\nENTER > ";
+    	cin >> answ;
+    	switch(answ){
+    		case 1: {
+    			if (head == 0 & tail == 0 ) {
+    				cout << "Queue is empty!\n";
+				} else {
+    				current = head;
+      				cout << "Queue: ";
+      				while (current != tail) {
+      					// виведення даних
+				        cout << arr[current++] << " ";
+				        // обнулення лічильника
+        				if (current > size) current = 0;
+      				}
+      				cout << endl;
+			    	if (abs(tail - head) == size) cout << "Queue is full!" << endl;
+				}
+				break; }
+			
+			case 2 : {
+				// перевірка на пустоту
+				if (head != tail) {
+					// виведення даних
+      				cout << "Deleted " << arr[head++] << endl; 
+      				// обнулення лічильника
+      				if (head > size) head = 0; 
+    			} else cout << "Queue is empty!" << endl;
+				break; }
+			
+			case 3: {
+				// перевірка на заповненість
+				if (abs(tail - head) != size) {
+					cout << "ENTER > ";
+					cin >> data;
+					// додавання в кінець
+      				arr[tail++] = data; 
+      				// обнуління лічильника
+      				if (tail > size) tail = 0;
+    			} else cout << "Queue is full!" << endl;
+				break; }
+		}
+	} while (answ != 0);
+	
+	// очищення пам'яті
+	delete[] arr;
+	return 0;
 }
